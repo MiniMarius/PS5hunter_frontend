17159bf9562071d7850661d88d0af1ffc0468018
MiniMarius <seffermarius@gmail.com>
2023-02-21 14:26:17 +0100

    state is now set on click

--- src/components/TableOfData.js
+++ src/components/TableOfData.js
@@ -9,7 +9,13 @@ import Paper from '@mui/material/Paper';
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
   }
-  
+
+
+const TableOfData = (products) => {
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
   const rows = [
     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
@@ -18,7 +24,6 @@ function createData(name, calories, fat, carbs, protein) {
     createData('Gingerbread', 356, 16.0, 49, 3.9),
   ];
 
-const TableOfData = () => {
     return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
--- src/view/StarterView.js
+++ src/view/StarterView.js
@@ -5,19 +5,19 @@ import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import TableOfData from "../components/TableOfData";
 import axios from "axios";
 function StarterView() {
-
-    const [product, setProduct] = React.useState(null)
+    const [products, setProducts] = React.useState(null)
     function handleClick() {
         axios.get("/api/product/")
-        .then((res) => console.log(res.data))
+        .then((res) => setProducts(res.data))
       .catch((err) => console.log(err));
     }
 
+    console.log(products)
     return (
         <div>
             <ResponsiveAppBar></ResponsiveAppBar>
             <Box display="flex" flexDirection="column" alignItems="stretch">
-                <TableOfData></TableOfData>
+                <TableOfData ></TableOfData>
                 <Button onClick={handleClick}>Fetch product</Button>
             </Box>
         </div>
