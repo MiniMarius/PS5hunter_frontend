981daebd65146988ca8e31f4cce51b5d0fae570c
MiniMarius <seffermarius@gmail.com>
2023-02-21 13:48:13 +0100

    api request now working against local api

--- src/view/StarterView.js
+++ src/view/StarterView.js
@@ -1,14 +1,24 @@
 import bigLogo from "../assets/ps5hunter.png";
-import React from "react";
+import React, { useEffect } from "react";
 import {Box, Button} from "@mui/material";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import TableOfData from "../components/TableOfData";
+import axios from "axios";
 function StarterView() {
+
+    const [product, setProduct] = React.useState(null)
+    function handleClick() {
+        axios.get("/api/product/")
+        .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+    }
+
     return (
         <div>
             <ResponsiveAppBar></ResponsiveAppBar>
             <Box display="flex" flexDirection="column" alignItems="stretch">
                 <TableOfData></TableOfData>
+                <Button onClick={handleClick}>Fetch product</Button>
             </Box>
         </div>
     )
