a6ce72bde44978b1c1ffbf51c5391a22df63b3d4
MiniMarius <seffermarius@gmail.com>
2023-02-22 13:54:42 +0100

    loading spinners now active during promises

--- src/view/StarterView.js
+++ src/view/StarterView.js
@@ -2,43 +2,50 @@ import bigLogo from "../assets/ps5hunter.png";
 import React, { useEffect } from "react";
 import {Box, Button} from "@mui/material";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Link from "@mui/material/Link";
 import axios from "axios";
 import ProductTable from "../components/ProductTable";
+import CircularProgress from "@mui/material/CircularProgress";
+import { flexbox } from "@mui/system";
 function StarterView() {
-
-    const [products, setProducts] = React.useState(null)
+    const [products, setProducts] = React.useState(null);
+    const [isLoading, setIsLoading] = React.useState(true);
+  
     useEffect(() => {
-        axios.get("/api/product/")
-        .then((res) => setProducts(res.data))
-      .catch((err) => console.log(err));
+      axios
+        .get("/api/product/")
+        .then((res) => {
+          setProducts(res.data);
+          setIsLoading(false);
+        })
+        .catch((err) => console.log(err));
     }, []);
-
+  
     const handleClick = () => {
-        axios.get('/run_scraper/')
-          .then(response => {
-            console.log(response.data);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      };
-
-    console.log(products)
+      setIsLoading(true);
+      axios
+        .get("/run_scraper/")
+        .then((response) => {
+          console.log(response.data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setIsLoading(false);
+        });
+    };
+  
     return (
-        <div>
-            <ResponsiveAppBar></ResponsiveAppBar>
-            <Box display="flex" flexDirection="column" alignItems="stretch">
-            <ProductTable products={products}></ProductTable>
-        <Button variant="contained" color="primary" onClick={handleClick}>Run Scraper</Button>
-            </Box>
-        </div>
-    )
-}
-
-export default StarterView;
+      <div>
+        <ResponsiveAppBar></ResponsiveAppBar>
+        <Box display="flex" flexDirection="column" alignItems="stretch">
+          <img src={bigLogo} alt="PS5 Hunter Logo" />
+          <Button variant="contained" color="primary" onClick={handleClick} disabled={isLoading}>
+            {isLoading ? <CircularProgress size={24} /> : "Run Scraper"}
+          </Button>
+          {isLoading ? (<Box display="flex" flexDirection="column" alignItems="center" padding={1}>Getting new data</Box>) : (<ProductTable products={products}></ProductTable>)}
+        </Box>
+      </div>
+    );
+  }
+  
+  export default StarterView;
