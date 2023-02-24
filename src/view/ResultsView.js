c87b67d649e7c170fcc15ec83b9982e473f6ece5
MiniMarius <seffermarius@gmail.com>
2023-02-24 08:28:44 +0100

    Now uses a grid layout with centered table of data

--- src/view/ResultsView.js
+++ src/view/ResultsView.js
@@ -5,29 +5,37 @@ import ProductTable from "../components/ProductTable";
 import CircularProgress from "@mui/material/CircularProgress";
 import backgroundImage from "../assets/frosty_wallpaper.jpg";
 import FrostyButton from '../components/FrostyButton';
-
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 function ResultsView({products, isLoading, handleClick}) {
-    return (
-      <Box style={{ backgroundImage: `url(${backgroundImage})`, minHeight: "100vh"}}>
-        <ResponsiveAppBar style={{ position: "fixed", width: "100%" }} />
-          <Box display="flex" flexDirection="column" alignItems="stretch" padding={2} marginTop={8}>
-            <Box flexDirection="column-reverse" alignItems="center">
-            <FrostyButton variant="contained" color="primary" onClick={handleClick} disabled={isLoading}>
-              {isLoading ? <CircularProgress size={24} /> : "Run Scraper"}
-            </FrostyButton>
-            </Box>
-            {isLoading ? (
-              <Box display="flex" flexDirection="column" alignItems="center" padding={1}>
-                Getting new data
+  return (
+    <Box style={{ backgroundImage: `url(${backgroundImage})`, minHeight: "100vh"}}>
+      <ResponsiveAppBar style={{ position: "fixed", width: "100%" }} />
+        <Box marginTop={8}>
+          <Grid container justifyContent="center" alignItems="center" style={{height: "100%"}}>
+            <Grid item xs={12} md={6}>
+              <Box display="flex" flexDirection="column" alignItems="center" padding={2}>
+                <Grid item>
+                  <Typography variant="h5" align="center" gutterBottom color="white">
+                    Scraped Products
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <FrostyButton variant="contained" color="primary" onClick={handleClick} disabled={isLoading}>
+                    {isLoading ? <CircularProgress size={24} /> : "Run Scraper"}
+                  </FrostyButton>
+                </Grid>
+                {!isLoading && (
+                  <Box marginTop={4} bgcolor="background.paper" p={2} width={1} maxWidth={800}>
+                    <ProductTable products={products} />
+                  </Box>
+                )}
               </Box>
-            ) : (
-              <Box marginLeft="auto" bgcolor="background.paper" p={2} width={1} maxWidth={500}>
-                <ProductTable products={products} />
-              </Box>
-            )}
-          </Box>
-      </Box>
-    );
-  }
-  
-  export default ResultsView;
+            </Grid>
+          </Grid>
+        </Box>
+    </Box>
+  );
+}
+
+export default ResultsView;
