9b4183cf6f16c802d733a6b86ec182151031cddf
MiniMarius <seffermarius@gmail.com>
2023-03-16 11:00:13 +0100

    api layer now uses auth token for requests

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
+import store from '../redux/store';
+
+const api = axios.create({
+  baseURL: '/api/',
+});
+
+// Add a request interceptor to include the JWT token in the Authorization header
+api.interceptors.request.use((config) => {
+  const { token } = store.getState().auth;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export const getProductData = async () => {
   try {
-    const response = await axios.get('/api/product/');
+    const response = await api.get('/product/');
     return response.data;
   } catch (error) {
     console.log(error);
@@ -11,7 +25,7 @@ export const getProductData = async () => {
 
 export const runScraper = async () => {
   try {
-    const response = await axios.post('/api/scraper/run/');
+    const response = await api.post('/scraper/run/');
     return response.data;
   } catch (error) {
     console.log(error);
